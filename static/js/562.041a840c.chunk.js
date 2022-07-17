180536b1631abfcaee9d366e2e7d321f8bb545fd
VolodymyrPlastun <89778643+VolodymyrPlastun@users.noreply.github.com>
2022-07-17 13:57:54 +0000

    Deploying to gh-pages from @ VolodymyrPlastun/goit-react-hw-08-phonebook@46cc0b12dabc2570aea0cade8ebf3d14badbc288 🚀

---
+++ static/js/562.041a840c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[562],{8562:function(e,n,o){o.r(n),o.d(n,{default:function(){return h}});var t=o(184);function h(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:"Hello, this is your home page"})})}}}]);
+//# sourceMappingURL=562.041a840c.chunk.js.map
